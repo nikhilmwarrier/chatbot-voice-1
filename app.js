e4cb8ff2873c5cd84014253dac5a1b10de22df16
nikhilmwarrier <champion.26nikhil@gmail.com>
2020-09-26 15:21:17 +0530

    Add keyboard input

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const button = document.querySelector(".talk");
-const content = document.querySelector(".content");
+const kbdButton = document.querySelector("#goText")
+const kbdIn  = document.querySelector("#textInput");
+const content = document.querySelector(".query");
+const response = document.querySelector(".response");
 
 
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -15,14 +18,14 @@ const greetings = [
   ];
   
 const smallTalk = [
-  "I am a bot. What is your name?",
-  "They call me a bot. What about you?"
+  "I am Zetta. What is your name?",
+  "They call me Zetta. What about you?"
   ] 
   
 
 recognition.onstart = function() {
   console.log("It's Yippee Time!");
-  content.innerHTML = "OK!";
+  content.innerHTML = "OK! Start Speaking!";
 };
 
 
@@ -40,10 +43,16 @@ recognition.onresult = function(event) {
 };
 
 
-// event listener to button
+// event listener to voice button
 
 button.addEventListener("click", () => {
   recognition.start();
+});
+
+// event listener to kbd button
+kbdButton.addEventListener("click", () => {
+  text = kbdIn.value;
+  ShowTranscriptOnly(text);
 });
 
 //output
@@ -52,26 +61,41 @@ function ReadOutLoud(message){
   
   const speech = new SpeechSynthesisUtterance();
   
-  // speech.text = "Didn't catch that, sorry!"; //default fallback
   
   
   if(message.includes('how are you')){
     const finalText = 
       greetings[Math.floor(Math.random() * greetings.length)];
     speech.text = finalText;
+    response.textContent = finalText;
   }
+
   
   else if (message.includes('what is your name')) {
     const finalText =
       smallTalk[Math.floor(Math.random() * smallTalk.length)];
     speech.text = finalText;
+    response.textContent = finalText;
     
   }
   
-  else if (message.includes('my name is nikhil || my name is Nikhil || I am Nikhil || I am nikhil')) {
-     const finalText = "Oh, Hello Nikhil!";
+  else if (message.includes('I am Nikhil')) {
+     const finalText = "Oh, Hello Nikhhil! How are you?";
      speech.text = finalText;
+    response.textContent =  "Oh, Hello Nikhil! How are you?";
+     
   }
+  
+  else if (message.includes('wish')) {
+    const finalText = message;
+    speech.text = finalText;
+    response.textContent = finalText;
+  
+  }
+  
+  // else{response.textContent = "Didn't catch that, sorry!";} //default fallback
+  
+  
   
   
   speech.volume = 1;
@@ -79,5 +103,30 @@ function ReadOutLoud(message){
   speech.pitch = 1.2;
   
   window.speechSynthesis.speak(speech);
+  
+}
+
+function ShowTranscriptOnly(message){
+  
+  if(message.includes('How are you')){
+    const finalText = 
+      greetings[Math.floor(Math.random() * greetings.length)];
+    response.textContent = finalText;
+  }
+
+  
+  else if (message.includes('What is your name')) {
+    const finalText =
+      smallTalk[Math.floor(Math.random() * smallTalk.length)];
+    response.textContent = finalText;
+    
+  }
+  
+  else if (message.includes('I am Nikhil')) {
+     const finalText = "Oh, Hello Nikhil! How are you?";
+    response.textContent =  finalText;
+     
+  }
+  
   
 }
